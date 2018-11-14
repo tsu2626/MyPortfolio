ee5f15ea80cb8f0e093f07fc9a5b6006ba659123
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-11-14 22:52:35 +0900

    create pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Top from '@/components/Top'
+import About from '@/components/About'
+import Work from '@/components/Work'
 
 Vue.use(Router)
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Top',
+      component: Top
+    },
+    {
+      path: '/about',
+      name: '',
+      component: About
+    },
+    {
+      path: '/work',
+      name: '',
+      component: Work
     }
   ]
 })
